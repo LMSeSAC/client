fc065d91a1119cc7792daace8df9397cbea93d50
ryeongse25 <seryeong971120@gmail.com>
2023-02-14 23:45:09 +0900

    feat: question form ui

--- src/components/classroom/element/form/Input.tsx
+++ src/components/classroom/element/form/Input.tsx
@@ -1,13 +1,23 @@
+import React, { KeyboardEvent } from "react";
+import "./Input.scss";
+
 interface InputT {
   label: string;
+  name: string;
+  onKeyDown?: (e: KeyboardEvent<HTMLInputElement>) => void;
 }
 
-export default function Input({ label }: InputT) {
+export default function Input({ label, name, onKeyDown }: InputT) {
   return (
     <>
       <div className="d-flex">
-        <span>{label}</span>
-        <input type="text" />
+        <span className="label">{label}</span>
+        <input
+          className="q-input"
+          type="text"
+          name={name}
+          onKeyDown={onKeyDown}
+        />
       </div>
     </>
   );
